f34c8cc30b03fbec52fc2118c4505efecc02ca29
Arthur Decovemacker <arthur.decovemacker@epitech.eu>
2023-05-23 13:26:51 +0200

    new contact

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -5,23 +5,19 @@
 <div class="h-screen bg-primary bg-gradient-to-tl from-secondary">
   <div class="py-10 px-72">
     <form action="/api/sendMail" method="POST" class="bg-white rounded space-y-2 p-6">
-      <div class="text-secondary font-bold text-center text-4xl">Contact us</div>
-
       <div>
-        <label for="email" class="block">Email:</label>
-        <input id="email" name="email" type="email" class="rounded border" required />
-      </div>
-      <div>
-        <label for="object" class="block">Object:</label>
-        <input id="object" name="object" type="text" class="rounded border" required />
-      </div>
-      <div>
-        <label for="message" class="block">Message:</label>
-        <textarea id="message" name="message" class="rounded w-full h-60 border" required />
+        <form action="https://formsubmit.co/your@email.com" method="POST">
+          <label for="email" class="block">Name:</label>
+          <input type="text" name="name" required>
+          <label for="email" class="block">Message:</label>
+          <input type="text" name="Message" required>
+          <label for="email" class="block">Email:</label>
+          <input type="email" name="email" required>
+          <button type="submit">Send</button>
+     </form>
       </div>
 
       <div class="text-center">
-        <input class="btn font-bold text-white py-5 px-12 rounded-lg shadow-lg bg-secondary" type="submit" value="Envoyer" data-modal-toggle="defaultModal"/>
       </div>
     </form>
   </div>
