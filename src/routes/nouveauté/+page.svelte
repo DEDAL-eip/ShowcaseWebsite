e034090308d52e778c65b221d31397e2fa87fcd8
Arthur Decovemacker <arthur.decovemacker@epitech.eu>
2023-07-19 19:18:19 +0200

    test

--- src/routes/nouveauté/+page.svelte
+++ src/routes/nouveauté/+page.svelte
@@ -4,10 +4,7 @@
 
 <div class="h-screen bg-primary bg-gradient-to-tl from-secondary">
   <div class="py-10 px-72">
-    <div class="font-bold absolute text-white">
-      <div>
-          Organised your trip, discover your city in one cl c
-      </div>
+    <div class="font-bold text-white text-center text-6xl">Our team:</div>
   </div>
   </div>
 </div>
