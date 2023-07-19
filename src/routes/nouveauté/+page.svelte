3aa9822cf31989b8dafef1684c7df83904c9f6ac
Arthur Decovemacker <arthur.decovemacker@epitech.eu>
2023-07-19 19:12:46 +0200

    test

--- src/routes/nouveauté/+page.svelte
+++ src/routes/nouveauté/+page.svelte
@@ -4,21 +4,10 @@
 
 <div class="h-screen bg-primary bg-gradient-to-tl from-secondary">
   <div class="py-10 px-72">
-    <form action="/api/sendMail" method="POST" class="bg-white rounded space-y-2 p-6">
+    <div class="font-bold absolute text-white text-6xl ">
       <div>
-        <form action="https://formsubmit.co/arthur.decovemacker@epitech.eu" method="POST">
-          <label for="email" class="block">Name:</label>
-          <input type="text" name="name" required>
-          <label for="email" class="block">Message:</label>
-          <input type="text" name="Message" required>
-          <label for="email" class="block">Email:</label>
-          <input type="email" name="email" required>
-          <button type="submit">Send</button>
-     </form>
+          Organised your trip, discover your city in one cl c
       </div>
-
-      <div class="text-center">
-      </div>
-    </form>
+  </div>
   </div>
 </div>
