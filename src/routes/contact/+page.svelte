c8c960f3534574cef4cf14b9b11893d7aa656d88
Arthur Decovemacker <arthur.decovemacker@epitech.eu>
2023-09-13 01:07:13 +0200

    modif button

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -8,7 +8,7 @@
     <form action="/api/sendMail" method="POST" class="bg-white rounded space-y-2 p-6">
       <div>
         <form action="https://formsubmit.co/arthur.decovemacker@epitech.eu" method="POST">
-          <label for="email" class="font-bold text-Black ">Name:</label>
+          <label for="email" class="block font-bold text-Black">Name:</label>
           <input type="text" name="name" required>
           <label for="email" class="block">Message:</label>
           <input type="text" name="Message" required>
