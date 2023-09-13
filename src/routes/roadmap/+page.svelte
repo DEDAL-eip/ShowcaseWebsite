22605c9a9c1677c591883f432f8c6545444566e7
Arthur Decovemacker <arthur.decovemacker@epitech.eu>
2023-09-14 00:24:21 +0200

    modif button

--- src/routes/roadmap/+page.svelte
+++ src/routes/roadmap/+page.svelte
@@ -12,5 +12,7 @@
           <ul class="text-white my-auto text-right space-y-12 text-l">
           </ul> 
       </div>
+      <div class="font-bold text-white text-center text-3xl">Qu'est-ce qu'une roadmap ?</div>
+      <div class="text-white text-xl">Une roadmap, ou feuille de route en français, est un outil de gestion de projet et de planification stratégique qui permet de définir et de communiquer les objectifs, les étapes clés et la vision d'un projet ou d'une entreprise sur une période donnée. C'est un document visuel qui présente de manière chronologique les différentes étapes à suivre pour atteindre les objectifs fixés.</div>
   </div>
 </div>
