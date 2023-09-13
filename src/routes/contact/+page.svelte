485175016acd0858b6de6cca3fc77576f0179505
Arthur Decovemacker <arthur.decovemacker@epitech.eu>
2023-09-13 15:25:18 +0200

    modif button

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -10,7 +10,7 @@
       <form target="_blank" action="https://formsubmit.co/arthur.decovemacker@epitech.eu" method="POST">
         <div class="form-group">
           <div class="form-row">
-            <div class="col">
+            <div class="col" style="text-align: center;">
               <input type="text" name="name" class="form-control" placeholder="Full Name" required>
             </div>
             <div class="col" style="text-align: center;" >
@@ -18,10 +18,10 @@
             </div>
           </div>
         </div>
-        <div class="form-group">
+        <div class="form-group" style="text-align: center;">
           <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
         </div>
-        <button type="submit" class="btn btn-lg btn-dark btn-block">Submit Form</button>
+        <button type="submit" style="text-align: center;" class="btn btn-lg btn-dark btn-block">Submit Form</button>
       </form>
     </div>
   </div>
