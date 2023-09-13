44aef778ebf2444243a44fd62e6b52c3bf25db0d
Arthur Decovemacker <arthur.decovemacker@epitech.eu>
2023-09-13 15:04:30 +0200

    modif button

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,22 +3,26 @@
 </svelte:head>
 
 
-<div class="container">
-  <h1>FormSubmit Demo</h1>
-  <form target="_blank" action="https://formsubmit.co/arthur.decovemacker@epitech.eu" method="POST">
-    <div class="form-group">
-      <div class="form-row">
-        <div class="col">
-          <input type="text" name="name" class="form-control" placeholder="Full Name" required>
+<div class="h-screen bg-primary bg-gradient-to-tl from-secondary">
+  <div class="py-10 px-72">
+    <div class="container">
+      <h1>Dedal Contact form</h1>
+      <form target="_blank" action="https://formsubmit.co/arthur.decovemacker@epitech.eu" method="POST">
+        <div class="form-group">
+          <div class="form-row">
+            <div class="col">
+              <input type="text" name="name" class="form-control" placeholder="Full Name" required>
+            </div>
+            <div class="col">
+              <input type="email" name="email" class="form-control" placeholder="Email Address" required>
+            </div>
+          </div>
         </div>
-        <div class="col">
-          <input type="email" name="email" class="form-control" placeholder="Email Address" required>
+        <div class="form-group">
+          <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
         </div>
-      </div>
+        <button type="submit" class="btn btn-lg btn-dark btn-block">Submit Form</button>
+      </form>
     </div>
-    <div class="form-group">
-      <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
-    </div>
-    <button type="submit" class="btn btn-lg btn-dark btn-block">Submit Form</button>
-  </form>
+  </div>
 </div>
