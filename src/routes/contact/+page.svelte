6a8be4c088db4e6a7cfd3efb589fe2753c27e9b6
Arthur Decovemacker <arthur.decovemacker@epitech.eu>
2023-09-13 14:52:25 +0200

    modif button

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,23 +3,22 @@
 </svelte:head>
 
 
-<div class="h-screen bg-primary bg-gradient-to-tl from-secondary">
-  <div class="py-10 px-72">
-    <form action="/api/sendMail" method="POST" class="bg-white rounded space-y-2 p-6">
-      <div>
-        <form action="https://formsubmit.co/arthur.decovemacker@epitech.eu" method="POST">
-          <label for="email" class="block font-bold text-Black">Name:</label>
-          <input type="text" name="name" required>
-          <label for="email" class="block font-bold text-Black">Message:</label>
-          <input type="text" name="Message" required>
-          <label for="email" class="block font-bold text-Black">Email:</label>
-          <input type="email" name="email" required>
-          <button type="submit" style="border-radius: 40px; border: 4px double #cccccc; background-color: midnightblue;" class="block font-bold text-White">Send</button>
-     </form>
+<div class="container">
+  <h1>FormSubmit Demo</h1>
+  <form target="_blank" action="https://formsubmit.co/arthur.decovemacker@epitech.eu" method="POST">
+    <div class="form-group">
+      <div class="form-row">
+        <div class="col">
+          <input type="text" name="name" class="form-control" placeholder="Full Name" required>
+        </div>
+        <div class="col">
+          <input type="email" name="email" class="form-control" placeholder="Email Address" required>
+        </div>
       </div>
-
-      <div class="text-center">
-      </div>
-    </form>
-  </div>
+    </div>
+    <div class="form-group">
+      <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
+    </div>
+    <button type="submit" class="btn btn-lg btn-dark btn-block">Submit Form</button>
+  </form>
 </div>
