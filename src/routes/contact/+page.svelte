eb262bb1aadf3863fdac2fe042574522a1154597
Arthur Decovemacker <arthur.decovemacker@epitech.eu>
2023-09-13 23:03:07 +0200

    modif button

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -5,7 +5,7 @@
 
 
 <div class="container">
-  <h1 style="margin-left: 700px;" class="font-bold text-white text-4xl">Dedal Contact form</h1>
+  <h1 style="margin-left: 600px;" class="font-bold text-white text-4xl">Dedal Contact form</h1>
   <form target="_blank" action="https://formsubmit.co/arthur.decovemacker@epitech.eu" method="POST">
     <div class="form-group">
       <div class="form-row">
@@ -20,6 +20,6 @@
     <div class="form-group" style="text-align: center;">
       <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
     </div>
-    <button type="submit" style="margin-left: 700px; background-color: ivory;" class="btn btn-lg btn-dark btn-block">Submit Form</button>
+    <button type="submit" style="margin-left: 700px; background-color: white;" class="btn btn-lg btn-dark btn-block">Submit Form</button>
   </form>
 </div>
